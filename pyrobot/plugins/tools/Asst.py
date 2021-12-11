3a3cc2198153987f13e7ea39910be57cb99ef888
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2021-12-11 21:47:01 +0530

    Update Asst.py

--- pyrobot/plugins/tools/Asst.py
+++ pyrobot/plugins/tools/Asst.py
@@ -4,8 +4,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, User, Mes
 @Client.on_message(filters.command('submit') & filters.private)
 async def report(bot, message):
         if message.reply_to_message:
-                                  await bot.send_message(chat_id=OWNER_ID, text=f"<b>⭕️NEW MESSAGE⭕️\n \n🧿 Name: {message.from_user.mention}\n🧿 User ID:</b> <code>{message.chat.id}</code>")
-                                  await bot.forward_messages(chat_id=OWNER_ID, from_chat_id=message.from_user.id, message_ids=message.reply_to_message.message_id)
+                                  await bot.send_message(chat_id=1857338892, text=f"<b>⭕️NEW MESSAGE⭕️\n \n🧿 Name: {message.from_user.mention}\n🧿 User ID:</b> <code>{message.chat.id}</code>")
+                                  await bot.forward_messages(chat_id=1857338892, from_chat_id=message.from_user.id, message_ids=message.reply_to_message.message_id)
                                   await message.reply_text("<b>✅ Your Feedback Successfully Submitted to the Admins</b>")
         else:
              await message.reply_text("<b>Use this command as the reply of any Message to Report</b>")
@@ -14,11 +14,11 @@ async def report(bot, message):
         
 @Client.on_message(filters.command('reply') & filters.private)
 async def replyt(bot, message):
-    if message.from_user.id == OWNER_ID: 
+    if message.from_user.id == 1857338892: 
                if message.reply_to_message:
                                     userid=int(message.text.replace("/reply"," "))
                                     await bot.send_message(chat_id=userid, text=f"<b>An Admin is responded to your feedback ✨</b>")
-                                    await bot.copy_message(chat_id=userid, from_chat_id=OWNER_ID, message_id=message.reply_to_message.message_id)
+                                    await bot.copy_message(chat_id=userid, from_chat_id=1857338892, message_id=message.reply_to_message.message_id)
                                     await message.reply_text("<b>✅ Your Reply Successfully Send to the User</b>")
                else:
                     await message.reply_text("<b>Use this command as the reply of any Message to Reply</b>")                         
