81715648d59a442e121711c7a011e76f8bbac83d
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2021-12-11 14:05:14 +0530

    Update Asst.py

--- pyrobot/plugins/tools/Asst.py
+++ pyrobot/plugins/tools/Asst.py
@@ -32,8 +32,8 @@ START_BUTTONS = InlineKeyboardMarkup(
 @Bot.on_message(filters.command('submit') & filters.private)
 async def report(bot, message):
         if message.reply_to_message:
-                                  await bot.send_message(chat_id=ADMIN, text=f"<b>⭕️NEW MESSAGE⭕️\n \n🧿 Name: {message.from_user.mention}\n🧿 User ID:</b> <code>{message.chat.id}</code>")
-                                  await bot.forward_messages(chat_id=ADMIN, from_chat_id=message.from_user.id, message_ids=message.reply_to_message.message_id)
+                                  await bot.send_message(chat_id=OWNER_ID, text=f"<b>⭕️NEW MESSAGE⭕️\n \n🧿 Name: {message.from_user.mention}\n🧿 User ID:</b> <code>{message.chat.id}</code>")
+                                  await bot.forward_messages(chat_id=OWNER_ID, from_chat_id=message.from_user.id, message_ids=message.reply_to_message.message_id)
                                   await message.reply_text("<b>✅ Your Feedback Successfully Submitted to the Admins</b>")
         else:
              await message.reply_text("<b>Use this command as the reply of any Message to Report</b>")
@@ -42,11 +42,11 @@ async def report(bot, message):
         
 @Bot.on_message(filters.command('reply') & filters.private)
 async def replyt(bot, message):
-    if message.from_user.id == ADMIN: 
+    if message.from_user.id == OWNER_ID: 
                if message.reply_to_message:
                                     userid=int(message.text.replace("/reply"," "))
                                     await bot.send_message(chat_id=userid, text=f"<b>An Admin is responded to your feedback ✨</b>")
-                                    await bot.copy_message(chat_id=userid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
+                                    await bot.copy_message(chat_id=userid, from_chat_id=OWNER_ID, message_id=message.reply_to_message.message_id)
                                     await message.reply_text("<b>✅ Your Reply Successfully Send to the User</b>")
                else:
                     await message.reply_text("<b>Use this command as the reply of any Message to Reply</b>")                         
@@ -57,10 +57,10 @@ async def replyt(bot, message):
                           
 @Bot.on_message(filters.command('send') & filters.private)
 async def send(bot, message):
-    if message.from_user.id == ADMIN: 
+    if message.from_user.id == OWNER_ID: 
                if message.reply_to_message:
                                     chatid=int(message.text.replace("/send"," "))
-                                    await bot.copy_message(chat_id=chatid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
+                                    await bot.copy_message(chat_id=chatid, from_chat_id=OWNER_ID, message_id=message.reply_to_message.message_id)
                                     await message.reply_text("<b>✅ Message Successfully Send to the Group</b>")
                else:
                     await message.reply_text("<b>Use this command as the reply of any Message to Send in Group</b>")                         
@@ -71,7 +71,7 @@ async def send(bot, message):
                                     
 @Bot.on_message(filters.command('pin') & filters.private)
 async def pin(bot, message):
-    if message.from_user.id == ADMIN: 
+    if message.from_user.id == OWNER_ID: 
                if message.reply_to_message:
                                     chatid=int(message.text.replace("/pin"," "))
                                     p=await bot.copy_message(chat_id=chatid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
