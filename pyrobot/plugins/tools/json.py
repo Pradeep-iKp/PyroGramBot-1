a69be16e8aba52f89bf766aa53374d993573e1b6
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2021-12-11 13:26:52 +0530

    Update json.py

--- pyrobot/plugins/tools/json.py
+++ pyrobot/plugins/tools/json.py
@@ -7,7 +7,7 @@ from pyrobot import COMMAND_HAND_LER
 from pyrobot.helper_functions.cust_p_filters import sudo_filter
 
 
-@Client.on_message(filters.command("json", COMMAND_HAND_LER) & sudo_filter)
+@Client.on_message(filters.command("json", COMMAND_HAND_LER))
 async def jsonify(_, message):
     the_real_message = None
     reply_to_id = None
