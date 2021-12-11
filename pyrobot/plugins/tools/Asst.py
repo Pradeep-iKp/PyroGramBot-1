b7ad25f1bd9e4bb76f66c8642c0cf70d421cfaad
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2021-12-11 22:29:14 +0530

    Update Asst.py

--- pyrobot/plugins/tools/Asst.py
+++ pyrobot/plugins/tools/Asst.py
@@ -3,9 +3,6 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, User, Message
 
-PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
-PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
-
 @Client.on_message(filters.command('submit') & filters.private)
 async def report(bot, message):
         if message.reply_to_message:
@@ -29,75 +26,3 @@ async def replyt(bot, message):
                     await message.reply_text("<b>Use this command as the reply of any Message to Reply</b>")                         
     else:
          await message.reply_text("<b>That's not for you bruh 😅</b>")
-
-                
-#-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
-
-@Client.on_message(filters.private & filters.text)
-async def pm_text(bot, message):
-    if message.from_user.id == 1857338892:
-        await reply_text(bot, message)
-        return
-    info = await bot.get_users(user_ids=message.from_user.id)
-    reference_id = int(message.chat.id)
-    await bot.send_message(
-        chat_id=1857338892,
-        text=PM_TXT_ATT.format(reference_id, info.first_name, message.text),
-        parse_mode="html"
-    )
-
-
-@Client.on_message(filters.private & filters.media)
-async def pm_media(bot, message):
-    if message.from_user.id == 1857338892:
-        await replay_media(bot, message)
-        return
-    info = await bot.get_users(user_ids=message.from_user.id)
-    reference_id = int(message.chat.id)
-    await bot.copy_message(
-        chat_id=1857338892,
-        from_chat_id=message.chat.id,
-        message_id=message.message_id,
-        caption=PM_MED_ATT.format(reference_id, info.first_name),
-        parse_mode="html"
-    )
-
-
-@Client.on_message(filters.user(1857338892) & filters.text)
-async def reply_text(bot, message):
-    reference_id = True
-    if message.reply_to_message is not None:
-        file = message.reply_to_message
-        try:
-            reference_id = file.text.split()[2]
-        except Exception:
-            pass
-        try:
-            reference_id = file.caption.split()[2]
-        except Exception:
-            pass
-        await bot.send_message(
-            text=message.text,
-            chat_id=int(reference_id)
-        )
-
-
-@Client.on_message(filters.user(1857338892) & filters.media)
-async def replay_media(bot, message):
-    reference_id = True
-    if message.reply_to_message is not None:
-        file = message.reply_to_message
-        try:
-            reference_id = file.text.split()[2]
-        except Exception:
-            pass
-        try:
-            reference_id = file.caption.split()[2]
-        except Exception:
-            pass
-        await bot.copy_message(
-            chat_id=int(reference_id),
-            from_chat_id=message.chat.id,
-            message_id=message.message_id,
-            parse_mode="html"
-        )
